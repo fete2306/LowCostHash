ab648aa0166587d89d3061347771bfe8e8abea80
fete2306 <2138218099@qq.com>
2025-11-24 20:28:57 +0800

    Add files via upload

--- LowCostHash.cpp
+++ LowCostHash.cpp
@@ -1,3 +1,4 @@
+//实现模板Indexs.但是效率低.而且取时也很麻烦,需要手动传入key,value的数据类型
 #include<iostream>
 #include<vector>
 #include<functional>
@@ -5,33 +6,27 @@
 #include<cstdint>
 #include<limits>
 
-#define UINT_MAX std::numeric_limits<unsigned int>::max()
-#define UINT8_MAX std::numeric_limits<uint8_t>::max()
-#define UINT16_MAX std::numeric_limits<uint16_t>::max()
-#define UINT32_MAX std::numeric_limits<uint32_t>::max()
-#define UINT64_MAX std::numeric_limits<uint64_t>::max()
-
-class Indexs{
+template<typename K>
+class Indexes{
 public:
 std::vector<int>data;
-size_t key;
+void* key;
 
-template<typename T>
-size_t hash(const T& value){//通用哈希函数
-    return std::hash<T>{}(value);
+template<typename k>
+size_t hash(const k& key){//通用哈希函数
+    return std::hash<k>{}(key);
     }
 
-void get_indexs(size_t key){
+void get_indexes(size_t key){
     while(key>0){
         this->data.push_back(key & 0b11);
         key>>=2;
         }
     }
-
-template<typename T>
-Indexs(const T& value){
-        get_indexs(hash(value));
-        this->key=hash(value);
+    
+Indexes(K key){
+    this->key=new K(key);     
+    this->get_indexes(hash(this->key));
     }
 void print(){
         std::cout<<"key:"<<this->key<<"\nsize:"<<this->data.size()<<"\ndata:"<<std::endl;
@@ -41,44 +36,55 @@ void print(){
     }    
 };
 
-
+template<typename T>
 class TableNode{
     public:
-    unsigned int p0=UINT_MAX;
-    unsigned int p1=UINT_MAX;
-    unsigned int p2=UINT_MAX;
-    unsigned int p3=UINT_MAX;
-    unsigned int value=UINT_MAX;
+    T p0=std::numeric_limits<T>::max();
+    T p1=std::numeric_limits<T>::max();
+    T p2=std::numeric_limits<T>::max();
+    T p3=std::numeric_limits<T>::max();
+    T value=std::numeric_limits<T>::max();
 };
 
+
+template<typename T>
 class Table{
     public:
-    std::vector<TableNode> Nodes;
+    std::vector<TableNode<T>> Nodes;
     std::vector<void*>Values;
+    T NodesMAX=std::numeric_limits<T>::max();
+
     Table() {
-        Nodes.push_back(TableNode()); 
+        Nodes.push_back(TableNode<T>()); 
     }
-    void print(){
-        TableNode* TempNode=nullptr;
-        std::cout<<"Nodes.size() = "<<Nodes.size()<<std::endl;
-        for(int i=0;i<Nodes.size();i++){
-            TempNode=&Nodes[i];
-            std::cout<<"Node["<<i<<"] p0="<<TempNode->p0<<" p1="<<TempNode->p1<<" p2="<<TempNode->p2<<" p3="<<TempNode->p3<<" value="<<TempNode->value<<std::endl;
+    template<typename goal>
+    Table<goal>* up(){
+        Table<goal>* newTable=new Table<goal>();
+        newTable->Nodes.reserve(this->Nodes.size());
+        for(int i=0;i<this->Nodes.size();i++){
+            TableNode<goal>Temp=TableNode<goal>();
+            Temp.p0=static_cast<goal>(this->Nodes[i].p0);
+            Temp.p1=static_cast<goal>(this->Nodes[i].p1);
+            Temp.p2=static_cast<goal>(this->Nodes[i].p2);
+            Temp.p3=static_cast<goal>(this->Nodes[i].p3);
+            Temp.value=static_cast<goal>(this->Nodes[i].value);
+            newTable->Nodes.push_back(Temp);
         }
-        std::cout<<"Values.size() = "<<Values.size()<<std::endl;
-        for(int i=0;i<Values.size();i++){
-            std::cout<<"Values["<<i<<"]="<<Values[i]<<std::endl;
-        }
+        newTable->Values=this->Values;
+        delete this;
+        return newTable;
     }
+    
     void shrink_to_fit(){
         Nodes.shrink_to_fit();
         Values.shrink_to_fit();
     }
-    void save(Indexs indexs,void* value){//需要确保void*指向的对象不被销毁
-        TableNode* TempNode=&Nodes[0];
-        for(int i=0;i<indexs.data.size();i++){
-            int index=indexs.data[i];
-            unsigned int* index_Node=nullptr;
+    template<typename K>
+    void save(Indexes<K> indexes,void* value){
+        TableNode<T>* TempNode=&Nodes[0];
+        for(int i=0;i<indexes.data.size();i++){
+            int index=indexes.data[i];
+            T* index_Node=nullptr;
             switch(index){
                 case 0:
                     index_Node=&TempNode->p0;
@@ -93,25 +99,30 @@ class Table{
                     index_Node=&TempNode->p3;
                     break;
                 default:
-                    std::cout<<"[save]索引越界"<<" indexs["<<i<<"]="<<index<<std::endl;
+                    std::cout<<"[save]索引越界"<<" indexes["<<i<<"]="<<index<<std::endl;
                     break;
             }
-            if(*index_Node==UINT_MAX){
+            if(*index_Node==this->NodesMAX){
                 *index_Node=Nodes.size();
-                Nodes.push_back(TableNode());
+                Nodes.push_back(TableNode<T>());
             }
             TempNode=&Nodes[*index_Node];
         }
+        if(TempNode==this->NodesMax){//当前未存值
         TempNode->value=Values.size();
-        Values.push_back(value);
+        Value v=Value(value,indexes.key);
+        Values.push_back(v);
+        }
+        else{
+            
+        }
     }
-
-    template<typename T>
-    T* get_value(Indexs indexs){
-        TableNode* TempNode=&Nodes[0];
-        for(int i=0;i<indexs.data.size();i++){
-            int index=indexs.data[i];
-            unsigned int* index_Node=nullptr;
+    template<typename K,typename V>
+    V* get(Indexes<K> indexes){
+        TableNode<T>* TempNode=&Nodes[0];
+        for(int i=0;i<indexes.data.size();i++){
+            int index=indexes.data[i];
+            T* index_Node=nullptr;
             switch(index){
                 case 0:
                     index_Node=&TempNode->p0;
@@ -126,39 +137,154 @@ class Table{
                     index_Node=&TempNode->p3;
                     break;
                 default:
-                    std::cout<<"[get]索引越界"<<" indexs["<<i<<"]="<<index<<std::endl;
+                    std::cout<<"[get]索引越界"<<" indexes["<<i<<"]="<<index<<std::endl;
                     break;
-            }
-            if(*index_Node==UINT_MAX){
-                *index_Node=Nodes.size();
-                Nodes.push_back(TableNode());
             }
             TempNode=&Nodes[*index_Node];
         }
-        return static_cast<T*>(Values[TempNode->value]);
+        return static_cast<V*>(Values[TempNode->value]);
+    }
+    template<typename k>
+    size_t hash(const k& value){//通用哈希函数
+        return std::hash<k>{}(value);
+    }
+    template<typename K>
+    Indexes<K> get_indexes(K key){
+        Indexes<K> indexes(key);
+        return indexes;
     }
 
-    template<typename T>
-    size_t hash(const T& value){//通用哈希函数
-        return std::hash<T>{}(value);
-    }
-
-    Indexs get_indexs(size_t key){
-        Indexs indexs(key);
-        return indexs;
-    }
-    
 };
 
+class LowCostHash{
+    public:
+    void* data;
+    uint8_t typeId=8;
+
+    LowCostHash(){
+        data=new Table<uint8_t>();
+    }
+    void shrink_to_fit(){
+        switch (this->typeId)
+        {
+        case 8:{
+            Table<uint8_t>* table=static_cast<Table<uint8_t>*>(this->data);
+            table->shrink_to_fit();
+            break;
+        }
+        case 16:{
+            Table<uint16_t>* table=static_cast<Table<uint16_t>*>(this->data);
+            table->shrink_to_fit();
+            break;
+        }
+        case 32:{
+            Table<uint32_t>* table=static_cast<Table<uint32_t>*>(this->data);
+            table->shrink_to_fit();
+            break;
+        }
+        case 64:{
+            Table<uint64_t>* table=static_cast<Table<uint64_t>*>(this->data);
+            table->shrink_to_fit();
+            break;    
+            }
+        default:
+        break;
+        }
+        }
+
+    template<typename K>
+    void save(Indexes<K> indexes,void* value){
+    switch (this->typeId)
+    {
+    case 8:{
+        Table<uint8_t>* table=static_cast<Table<uint8_t>*>(this->data);
+        if(table->Nodes.size()-1+indexes.data.size()>=table->NodesMAX){
+            this->data=table->up<uint16_t>();
+            this->typeId=16;
+            this->save<K>(indexes,value);
+            return;
+        }
+        table->save<K>(indexes,value);
+        break;
+    }
+    case 16:{
+        Table<uint16_t>* table=static_cast<Table<uint16_t>*>(this->data);
+        if(table->Nodes.size()-1+indexes.data.size()>=table->NodesMAX){
+            this->data=table->up<uint32_t>();
+            this->typeId=32;
+            this->save<K>(indexes,value);
+            return;
+        }
+        table->save<K>(indexes,value);
+        break;
+    }
+        
+    case 32:{
+        Table<uint32_t>* table=static_cast<Table<uint32_t>*>(this->data);
+        if(table->Nodes.size()-1+indexes.data.size()>=table->NodesMAX){
+            this->data=table->up<uint64_t>();
+            this->typeId=64;
+            this->save<K>(indexes,value);
+            return;
+        }
+        table->save<K>(indexes,value);
+        break;
+    }
+    case 64:{
+        Table<uint64_t>* table=static_cast<Table<uint64_t>*>(this->data);
+        table->save<K>(indexes,value);
+        break;    
+        }
+    
+    default:
+        break;
+    }   
+    }
+
+    template<typename K,typename V>
+    V* get(Indexes<K> indexes){
+    switch (this->typeId)
+    {
+    case 8:{
+        Table<uint8_t>* table=static_cast<Table<uint8_t>*>(this->data);
+        return table->get<K,V>(indexes);
+        break;
+    }
+    case 16:{
+        Table<uint16_t>* table=static_cast<Table<uint16_t>*>(this->data);
+        return table->get<K,V>(indexes);
+        break;
+    }
+        
+    case 32:{
+        Table<uint32_t>* table=static_cast<Table<uint32_t>*>(this->data);
+        return table->get<K,V>(indexes);
+        break;
+    }
+    case 64:{
+        Table<uint64_t>* table=static_cast<Table<uint64_t>*>(this->data);
+        return table->get<K,V>(indexes);
+        break;    
+        }
+    
+    default:
+        std::cout<<"get failed"<<std::endl;
+        return nullptr;
+        break;
+    }   
+    }
+
+};
+
+
 int main(){
-    Table table=Table();
-    Indexs indexs=Indexs(1101);
-    indexs.print();
+    LowCostHash table=LowCostHash();
+    Indexes<int> indexes(1101);
+    indexes.print();
     std::string value="hello";
-    table.save(indexs,&value);
-    std::cout<<"the value is "<<*(table.get_value<std::string>(indexs))<<std::endl;
+    table.save(indexes,&value);
+    std::cout<<"the value is "<<*(table.get<int,std::string>(indexes))<<std::endl;
     table.shrink_to_fit();
-    table.print();
     return 0;
 
 }
